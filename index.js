cf448fac21fd02445faabb965438119f514bd221
nahcg <glor.chan@mail.utoronto.ca>
2023-05-27 22:57:30 -0400

    add fetchISS test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
-
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -11,11 +11,21 @@ fetchMyIP((error, ip) => {
   console.log('It worked! Returned IP:' , ip);
 });
 
-fetchCoordsByIP('42', (error, latlong) => {
+fetchCoordsByIP('8.8.4.4', (error, latlong) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
   console.log('It worked! Returned coordinates:' , latlong);
+});
+
+
+fetchISSFlyOverTimes({ latitude: 43.653226, longitude: -79.3831843 }, (error, resp) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned responses:' , resp);
 });
